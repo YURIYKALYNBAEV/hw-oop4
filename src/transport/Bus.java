3f43eaee6dfd1ff1218a84332da3b29eddae7227
YURIYKALYNBAEV <amber3200@mail.ru>
2023-01-12 19:06:36 +0300

    ООП.Часть 4.Домашнее задание.

--- src/transport/Bus.java
+++ src/transport/Bus.java
@@ -25,7 +25,7 @@ public class Bus extends Transport <DriverD>{
 
     @Override
     public void BestLapTime() {
-        int minBound = 100;
+        int minBound = 90;
         int maxBound = 150;
         int bestLapTime = (int) ((int) (maxBound - minBound) * Math.random());
         System.out.println("Лучшее время круга для автобуса в минутах" + bestLapTime);
@@ -33,7 +33,7 @@ public class Bus extends Transport <DriverD>{
 
     @Override
     public void MaximumSpeed() {
-        int minBound = 80;
+        int minBound = 70;
         int maxBound = 120;
         int maximumSpeed = (int) ((int) (maxBound - minBound) * Math.random());
         System.out.println("Максимальная скорость для автобуса" + maximumSpeed);
